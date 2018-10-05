e8af589e2ca428613a82f66ffb85bfc0ccecbf85
Szabo Peter <p.szabo@mito.hu>
2018-10-05 12:33:24 +0200

    Removed unnecessary configs

--- configs/index.js
+++ configs/index.js
@@ -13,18 +13,7 @@ const configs = {
   db: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-    dialectOptions: {
-      charset: 'utf8',
-      collate: 'utf8_general_ci',
-    },
-    migrationStorageTableName: 'sequelize_meta',
-    operatorsAliases: false,
-    define: {
-      underscored: true,
-    },
   },
 };
 
